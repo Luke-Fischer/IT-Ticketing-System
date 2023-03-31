b0b34575eff4563d5c519fb2ddf12ee1d1a85264
lfischer <lfischer@uoguelph.ca>
2023-03-31 17:36:47 -0400

    added front end for login screen

---
+++ IT Ticketing System/Controllers/EmployeeSignUpController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IT_Ticketing_System.Controllers
+{
+    public class EmployeeSignUpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
