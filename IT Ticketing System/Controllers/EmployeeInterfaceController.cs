0644581dc4dff5d7f7bb85f0403ba782e8a7b8c1
lfischer <lfischer@uoguelph.ca>
2023-04-08 19:13:09 -0400

    Users now have their own interface displaying only their tickets

--- IT Ticketing System/Controllers/EmployeeInterfaceController.cs
+++ IT Ticketing System/Controllers/EmployeeInterfaceController.cs
@@ -1,36 +1,57 @@
 ﻿using IT_Ticketing_System.Data;
 using IT_Ticketing_System.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace IT_Ticketing_System.Controllers
 {
     public class EmployeeInterfaceController : Controller
     {
         private UserDbContext _db;
+        private static int _userId;
         public EmployeeInterfaceController(UserDbContext db)
         {
             _db = db;
         }
         public IActionResult Index(User obj)
         {
-            IEnumerable<User> objCategoryList = _db.Users;
-            foreach(User user in objCategoryList)
+            Debug.WriteLine(obj.Email + "\n\n\n\n");
+
+            var userList = _db.Users.ToList();
+            foreach (User tempUser in userList)
             {
-                if(obj.Email == user.Email)
+                if (tempUser.Email == obj.Email)
                 {
-					//get ID
-					obj.Id = user.Id;
+                    _userId = tempUser.Id;
                 }
             }
-            IEnumerable<Ticket> ticketList = _db.Tickets.Where(p => p.UserId == obj.Id);
+            
+
+            IEnumerable<Ticket> ticketList = _db.Tickets.Where(p => p.UserId == _userId);
             return View(ticketList);
 
         }
 
-        //GET
-        public IActionResult Create()
+		//GET
+		public IActionResult Create()
+		{
+
+			return View();
+		}
+
+		//POST
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create(Ticket obj)
         {
-            return View();
+            obj.Status = "pending";
+            obj.UserId = _userId;
+            Debug.WriteLine(obj.Title + obj.Message + obj.Priority + obj.timeSubmitted + obj.UserId);
+			//Push to db
+			_db.Tickets.Add(obj);
+			_db.SaveChanges();
+			TempData["success"] = "Ticket created successfully";
+			return RedirectToAction("Index");
         }
     }
 }
